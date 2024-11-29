b7a0fbcbff59e27c5bec160d742216b637d823ab
EricSeguraG <ersego@prateducacio.com>
2024-11-29 16:11:08 +0100

    Create parellsenars.java

---
+++ AEA1/parellsenars.java
@@ -0,0 +1,27 @@
+public class parellsenars {
+        public static void main(String[] args) {
+           
+            int [][] parellsenars = new int[10][10];
+    
+            for(int i = 0; i < parellsenars.length; i++) {
+                for(int j = 0; j < parellsenars[i].length; j++) {
+                    if (i % 2 == 0 && j % 2 == 0)
+                        parellsenars[i][j] = 1;
+                    if (i % 2 ==0 && j % 2 !=0)
+                        parellsenars[i][j] = 2;
+                    if (i % 2 !=0 && j %2 ==0)
+                        parellsenars[i][j] = 3;
+                    if (i % 2 !=0 && j % 2 !=0)
+                        parellsenars[i][j] = 4;
+                    System.out.print(parellsenars[i][j]+ " ");
+                }
+                System.out.println();
+                
+    
+            }
+    
+            
+            
+        }
+
+}
